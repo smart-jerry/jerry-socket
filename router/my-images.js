ae359715d913308de5daebc59117490340c197c2
pengqin <jerry@jollycorp.com>
2018-11-07 14:56:14 +0800

    readme

--- router/my-images.js
+++ router/my-images.js
@@ -4,11 +4,25 @@
 var express = require('express');
 var router = express.Router();
 router.get('/getImage', function(req, res){
-	console.log(req.baseUrl,'===============');
+	console.log(req,'===============');
 //	res.sendFile(__dirname + 'index.html');
 	var baseInfo ={
 		a:'a'
 	}
 	res.render('my-images', {data: baseInfo});
 });
+
+
+// 图片上传请求
+router.post('/get/image',function (request, response) {
+	console.log(request.body,'88888888888888888888800000000000000000');
+//	res.send({
+//		'result':0,
+//		'message':'success'
+//	});
+//	response.render('my-images', {data: {
+//		'result':0,
+//		'message':'success'
+//	}});
+})
 module.exports = router;
